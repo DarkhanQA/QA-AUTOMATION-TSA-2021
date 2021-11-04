fab35b109e7df67c33491237e01f8ccc0f7606c4
Darkhan Khamid <darkhan@Darkhans-MBP.fios-router.home>
2021-11-03 21:54:14 -0400

    First Commit

---
+++ src/HomeWorks/Test_SandBox.java
@@ -0,0 +1,8 @@
+package HomeWorks;
+
+public class Test_SandBox {
+    public static void main(String[] args) {
+        System.out.println("\nHello World!");
+    }
+
+}
---
+++ src/StudyClassPackage/HomeWork_2_Temperature_Conversions.java
@@ -0,0 +1,7 @@
+package StudyClassPackage;
+
+public class HomeWork_2_Temperature_Conversions {
+        public static void main(String[] args) {
+
+        }
+}
